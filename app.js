a297d3ccc81b944faef47deb6de03cf84ff62e50
Razvan <razvan.idnc@gmail.com>
2021-07-10 00:41:41 +0300

    New Commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let btn = document.querySelectorAll(".accordion");
 let qst = document.querySelectorAll(".question");
 
 
-for (let i = 0; i <= btn.length; i++) {
+for (let i = 0; i < btn.length; i++) {
     btn[i].addEventListener("click", () => {
         // console.log("It works");
         btn[i].classList.toggle("show");
